5a8ba40e96de99e5f0e0c7c68a25f165faf2fcf9
Arthur <wangzhiqing@ata.net.cn>
2013-01-06 14:47:50 +0800

    django add template cache

--- djapps/djapps/settings.py
+++ djapps/djapps/settings.py
@@ -86,9 +86,11 @@ SECRET_KEY = '#$fml)w4&amp;b%!v!#%ysy1=ct+sih-++&amp;mx%&amp;n61op(umj98-%sw'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
+      )),
 )
 
 MIDDLEWARE_CLASSES = (
