41e505bcbbec48be1c1e91047f6bf1007fd372ec
Arthur <wangzhiqing@ata.net.cn>
2013-01-06 16:20:01 +0800

    add time calc

--- ttornado/app.py
+++ ttornado/app.py
@@ -1,10 +1,18 @@
 import tornado.ioloop
 import tornado.web
 import tornado.process
+import time
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world from tornado")
+        t1 = time.time()
+        for i in range(100000):
+            a = 'a'*100
+            b = 'b'*100
+            c = a+b
+        t2 = (time.time() - t1) * 1000
+
+        self.write("Hello, world from tornado: %s ms"%t2)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
