572448e5d015ca790c05efe13c0b9469d499cc77
Arthur <wangzhiqing@ata.net.cn>
2013-01-06 16:53:08 +0800

    add ttornado template

--- ttornado/app.py
+++ ttornado/app.py
@@ -2,6 +2,13 @@ import tornado.ioloop
 import tornado.web
 import tornado.process
 import time
+from jinja2 import Template
+from jinja2 import Environment, PackageLoader, FileSystemLoader
+import os
+
+curpath = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(curpath, 'templates')
+jinja_env = Environment(loader=FileSystemLoader(templates_dir))
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -14,8 +21,13 @@ class MainHandler(tornado.web.RequestHandler):
 
         self.write("Hello, world from tornado: %s ms"%t2)
 
+class LoopTempHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(jinja_env.get_template('loop_t.html').render())
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/t", LoopTempHandler),
 ])
 
 if __name__ == "__main__":
